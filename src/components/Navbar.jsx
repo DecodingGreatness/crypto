535c0223bcbd5882fa58360af1f095f710e01540
Edward Ho <pythoned77@gmail.com>
2022-08-31 12:51:33 +0800

    added BrowserRouter and Router in index.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,19 @@ import {
   MenuOutlined,
 } from "@ant-design/icons";
 
+import icon from "../images/cryptocurrency.png";
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Crytoverse</Link>
+        </Typography.Title>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
