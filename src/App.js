37fe4e95c56dd6b970b53f94e57295ff5b5f7bc5
Edward Ho <pythoned77@gmail.com>
2022-08-31 13:23:06 +0800

    added menu items onto Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
-import { Layout, Typography, Space } from "antd";
+import { Layout, Typography, Space, Menu } from "antd";
 
 import { Navbar } from "./components";
+import "./App.css";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
 
 const App = () => {
   return (
