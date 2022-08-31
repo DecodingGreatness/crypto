145fd39692d0a6487589f9df3d04c4d6d7a57ea7
Edward Ho <pythoned77@gmail.com>
2022-08-31 18:06:36 +0800

    resolved bug for coinId

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
+import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
+
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CyptoDetails = () => {
-  return <div>CyptoDetails</div>;
+  const { uuid } = useParams();
+  console.log(uuid);
+  const [timePeriod, setTimePeriod] = useState("7d");
+  const { data, isFetching } = useGetCryptoDetailsQuery(uuid);
+
+  return <div>CyptoDetails {uuid}</div>;
 };
 
 export default CyptoDetails;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -16,7 +16,10 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
+    getCryptoDetails: builder.query({
+      query: (uuid) => createRequest(`/coin/${uuid}`),
+    }),
   }),
 });
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
