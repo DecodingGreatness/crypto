352bb71b8acf17cab405358e73e4a2a4d6c3bfed
Edward Ho <pythoned77@gmail.com>
2022-09-01 10:24:40 +0800

    filled up coin details with stats

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -70,7 +70,7 @@ const CryptoDetails = () => {
       icon: <MoneyCollectOutlined />,
     },
     {
-      title: "Aprroved Supply",
+      title: "Approved Supply",
       value: cryptoDetails?.supply?.confirmed ? (
         <CheckOutlined />
       ) : (
@@ -131,6 +131,46 @@ const CryptoDetails = () => {
                 </Col>
                 <Text className="stats">{value}</Text>
               </Col>
+            ))}
+          </Col>
+          <Col className="other-stats-info">
+            <Col className="coin-value-statistic-heading">
+              <Title level={3} className="coin-details-heading">
+                Other Statistics
+              </Title>
+              <p>An overview showing the stats of all cryptocurrencies</p>
+            </Col>
+            {genericStats.map(({ icon, title, value }) => (
+              <Col className="coin-stats">
+                <Col className="coin-stats-name">
+                  <Text>{icon}</Text>
+                  <Text>{title}</Text>
+                </Col>
+                <Text className="stats">{value}</Text>
+              </Col>
+            ))}
+          </Col>
+        </Col>
+        <Col className="coin-desc-link">
+          <Row className="coin-desc">
+            <Title level={3} className="coin-details-heading">
+              What is {cryptoDetails.name}
+              {HTMLReactParser(cryptoDetails.description)}
+            </Title>
+          </Row>
+          <Col className="coin-links">
+            <Title level={3} className="coin-details-heading">
+              {cryptoDetails.name} Links
+            </Title>
+            {cryptoDetails.links.map((link) => (
+              <Row className="coin-link" key={link.name}>
+                <Title level={5} className="link-name">
+                  {link.type}
+                </Title>
+                <a href={link.url} target="_blank" rel="noreferr">
+                  {link.name}
+                </a>
+              </Row>
             ))}
           </Col>
         </Col>
